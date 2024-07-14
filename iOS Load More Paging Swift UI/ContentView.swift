f9aeefb8ca74d4eacb58b292333ae50acd522c13
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-07-14 20:43:23 +0700

    :tada: initial commit

---
+++ iOS Load More Paging Swift UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Load More Paging Swift UI
+//
+//  Created by Yusuf Saifudin on 14/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Load More Paging Swift UI/iOS_Load_More_Paging_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Load_More_Paging_Swift_UIApp.swift
+//  iOS Load More Paging Swift UI
+//
+//  Created by Yusuf Saifudin on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Load_More_Paging_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
